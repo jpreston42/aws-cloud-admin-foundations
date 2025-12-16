d0f8ee2aacbd258985746729067029d049f9e632
jpreston42 <jpreston4286@gmail.com>
2025-12-16 13:19:10 -0800

    Update main.tf
    Add internet gateway and public route table

--- terraform/01-network-foundation/main.tf
+++ terraform/01-network-foundation/main.tf
@@ -37,3 +37,34 @@ resource "aws_subnet" "public" {
     Env     = "lab"
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name    = "vpc-igw"
+    Project = "aws-cloud-admin-foundations"
+    Env     = "lab"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name    = "public-rt"
+    Project = "aws-cloud-admin-foundations"
+    Env     = "lab"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
