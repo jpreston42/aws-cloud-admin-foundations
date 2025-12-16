3030125853d60e1a26c399be4a8859974104e5d2
jpreston42 <jpreston4286@gmail.com>
2025-12-16 13:12:09 -0800

    Update main.tf
    Add base VPC with DNS support

--- terraform/01-network-foundation/main.tf
+++ terraform/01-network-foundation/main.tf
@@ -13,3 +13,14 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name    = "cloud-admin-foundations-vpc"
+    Project = "aws-cloud-admin-foundations"
+    Env     = "lab"
+  }
+}
